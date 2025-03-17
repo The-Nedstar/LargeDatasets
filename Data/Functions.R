c5142b2e8838c0af99653da70d544cd106bd14a3
The_Tedstar <edward.aplin@st-annes.ox.ac.uk>
2025-03-17 16:46:28 +0000

    draft 1

--- Data/Functions.R
+++ Data/Functions.R
@@ -3,26 +3,27 @@
 # author: anonymous
 ###########################################
 
-## creation of a histogram
+### creation of a histogram
 histogram <- function(Data, Xaxis, Xtitle, Title, BW, Line, File){
+  ## defining the histogram
   temp <- ggplot(Data, aes(x = Xaxis)) +
-    geom_histogram(aes(fill = as.factor(floor(..x.. / BW) %% 2)),
-                   binwidth = BW, boundary = 1, 
-                   colour = "black", size = 0.4)+
-    (if (Line == TRUE) {
+    geom_histogram(aes(fill = as.factor(floor(..x.. / BW) %% 2)), # alternative colours
+                   binwidth = BW, boundary = 1, # ensuring it is split at the value 1
+                   colour = "black", size = 0.4) +
+    (if (Line == TRUE) { # only carrying out if specified
       geom_vline(xintercept = 1, colour = "red", 
-                 size = 1.3, linetype = "21")
-    }) +
-    scale_fill_manual(values = c("#707070", "#555555")) +
+                 size = 1.3, linetype = "21")}) + # creates a vertical line at the value 1
+    scale_fill_manual(values = c("#707070", "#555555")) + # specifying alternating colours
     theme_bw()+
     theme(
-      legend.position = "none",
+      legend.position = "none", # removing the legend
       axis.title = element_text(size = 14),
       title = element_text(size = 14)
     ) +
     xlab(Xtitle) +
     ylab("Frequency") +
     ggtitle(Title)
+  ## saving the graph as a .svg
   svglite(here("Figures", File), width = 10,
           height = 6,
           scaling = 1.1)
@@ -30,22 +31,24 @@ histogram <- function(Data, Xaxis, Xtitle, Title, BW, Line, File){
   dev.off()
 }
 
-## create a scatterplot
+### create a scatterplot
 scatterplot <- function(Data, Xaxis, Xtitle, Yaxis, Ytitle, Title, Line, File){
+  ## creating the scatterplot
   temp <- ggplot(Data, aes(x = Xaxis, y = Yaxis)) +
     geom_point(colour = "darkblue")+
-    (if (Line == TRUE) {
-      geom_smooth(method = "lm", se = TRUE, linetype = "solid", colour = "darkred")
-    }) +
+    (if (Line == TRUE) { # Only running if specified
+      geom_smooth(method = "lm", se = TRUE, linetype = "solid", 
+                  colour = "darkred")}) + # creating a line defined by a generalised linear model
     theme_bw()+
     theme(
-      legend.position = "none",
+      legend.position = "none", # removing the legend
       axis.title = element_text(size = 14),
       title = element_text(size = 14)
     ) +
     xlab(Xtitle) +
     ylab(Ytitle) +
     ggtitle(Title)
+  ## saving as a .svg
   svglite(here("Figures", File), width = 10,
           height = 6,
           scaling = 1.1)
@@ -55,32 +58,39 @@ scatterplot <- function(Data, Xaxis, Xtitle, Yaxis, Ytitle, Title, Line, File){
 
 ### Create Diagnostic plots
 diagnostic_plots <- function(File, Model) {
+  ## saving plot as an SVG
   svglite(here("Figures", File), width = 10,
           height = 8,
           scaling = 1.3)
+  ## creating a multi plot
+  # defining multi plot layout
   par(mfrow=c(2,2))
+  # combining each diagnostic plot into one multi plot
   DiaPlots <- (plot(Model,1) | plot(Model,2)) / (plot(Model,3) | plot(Model,5))
   dev.off()
 }
 
-## create a boxplot
+### create a boxplot
 boxplot <- function(Data, Xaxis, Xtitle, Yaxis, Ytitle, Title, File){
+  ## defining the boxplot
   temp <- ggplot(Data, aes(x = Xaxis, y = Yaxis, fill = Xaxis)) +
     geom_boxplot() +
     geom_beeswarm(alpha = 0.6, size = 0.75, corral.width = 0.5) +
-    scale_fill_manual(values = c("#E89494", "#A4DFE4", "#8082E4", "#F2D16F", "#EC89E4")) +
+    scale_fill_manual(values = c("#E89494", "#A4DFE4", "#8082E4",
+                                 "#F2D16F", "#EC89E4")) + # defining colours
     theme_bw() +
     theme(
-      legend.position = "none",
+      legend.position = "none", # removing the legend
       axis.title = element_text(size = 14),
       title = element_text(size = 14)
     ) +
     xlab(Xtitle) +
     ylab(Ytitle) +
     ggtitle(Title)
+  ## saving as a .svg
   svglite(here("Figures", File), width = 10,
           height = 6,
-          scaling = 1.1)
+          scaling = 1.3)
   print(temp)
   dev.off()
 }
